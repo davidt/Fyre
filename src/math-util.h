234ebd99e917517ea82bb89f781baf581f01e196
Micah Elizabeth Scott <beth@scanlime.org>
2004-02-27 00:32:42 +0000

    silly silly scanline
    git-svn-id: http://svn.navi.cx/misc/trunk/chaos/de-jong-explorer@2881 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ src/math-util.h
@@ -0,0 +1,32 @@
+/*
+ * math-util.h - Small math utilities shared by other modules
+ *
+ * de Jong Explorer - interactive exploration of the Peter de Jong attractor
+ * Copyright (C) 2004 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#ifndef __MATH_UTIL_H__
+#define __MATH_UTIL_H__
+
+float uniform_variate();
+float normal_variate();
+int find_upper_pow2(int x);
+
+#endif /* __MATH_UTIL_H__ */
+
+/* The End */
