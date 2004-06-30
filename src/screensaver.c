28340e7e80f25930e9189be03124f2d634c9279a
Micah Elizabeth Scott <beth@scanlime.org>
2004-06-30 04:05:04 +0000

    Oh, maybe I should add this file...
    git-svn-id: http://svn.navi.cx/misc/trunk/fyre@4595 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ src/screensaver.c
@@ -0,0 +1,158 @@
+/*
+ * screensaver.c - A self-running Fyre screen saver
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#include "screensaver.h"
+#include "histogram-view.h"
+
+static void screensaver_class_init(ScreenSaverClass *klass);
+static void screensaver_init(ScreenSaver *self);
+static void screensaver_dispose(GObject *gobject);
+
+static int screensaver_idle_handler(gpointer user_data);
+
+
+/************************************************************************************/
+/**************************************************** Initialization / Finalization */
+/************************************************************************************/
+
+GType screensaver_get_type(void) {
+  static GType exp_type = 0;
+
+  if (!exp_type) {
+    static const GTypeInfo dj_info = {
+      sizeof(ScreenSaverClass),
+      NULL, /* base_init */
+      NULL, /* base_finalize */
+      (GClassInitFunc) screensaver_class_init,
+      NULL, /* class_finalize */
+      NULL, /* class_data */
+      sizeof(ScreenSaver),
+      0,
+      (GInstanceInitFunc) screensaver_init,
+    };
+
+    exp_type = g_type_register_static(G_TYPE_OBJECT, "ScreenSaver", &dj_info, 0);
+  }
+
+  return exp_type;
+}
+
+static void screensaver_class_init(ScreenSaverClass *klass) {
+  GObjectClass *object_class = (GObjectClass*) klass;
+
+  object_class->dispose = screensaver_dispose;
+}
+
+static void screensaver_init(ScreenSaver *self) {
+}
+
+static void screensaver_dispose(GObject *gobject) {
+  ScreenSaver *self = SCREENSAVER(gobject);
+
+  if (self->map) {
+    g_object_unref(self->map);
+    self->map = NULL;
+  }
+  if (self->animation) {
+    g_object_unref(self->animation);
+    self->animation = NULL;
+  }
+  if (self->idler) {
+    g_source_remove(self->idler);
+    self->idler = 0;
+  }
+}
+
+ScreenSaver* screensaver_new(IterativeMap *map, Animation *animation) {
+  ScreenSaver *self = SCREENSAVER(g_object_new(screensaver_get_type(), NULL));
+  int i;
+  AnimationIter iter;
+  gchar* common_parameters;
+
+  self->animation = ANIMATION(g_object_ref(animation));
+  self->map = ITERATIVE_MAP(g_object_ref(map));
+
+  /* Create the GUI */
+  self->window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+  gtk_window_set_title(GTK_WINDOW(self->window), "Fyre Screensaver");
+  self->view = histogram_view_new(HISTOGRAM_IMAGER(self->map));
+  gtk_container_add(GTK_CONTAINER(self->window), self->view);
+  gtk_widget_show_all(self->window);
+
+  /* Allocate and interpolate all frames */
+  self->framerate = 10;
+  self->num_frames = animation_get_length(self->animation) * self->framerate;
+  self->frame_renders = g_new0(IterativeMap*, self->num_frames);
+  self->frame_parameters = g_new0(ParameterHolderPair, self->num_frames);
+  self->current_frame = 0;
+  common_parameters = parameter_holder_save_string(PARAMETER_HOLDER(map));
+
+  animation_iter_seek(animation, &iter, 0);
+  for (i=0; i<self->num_frames; i++) {
+    self->frame_renders[i] = ITERATIVE_MAP(de_jong_new());
+    parameter_holder_load_string(PARAMETER_HOLDER(self->frame_renders[i]), common_parameters);
+
+    self->frame_parameters[i].a = PARAMETER_HOLDER(de_jong_new());
+    animation_iter_load(animation, &iter, self->frame_parameters[i].a);
+    animation_iter_seek_relative(animation, &iter, 1/self->framerate);
+  }
+  for (i=0; i<self->num_frames-1; i++)
+    self->frame_parameters[i].b = self->frame_parameters[i+1].a;
+  self->frame_parameters[self->num_frames-1].b = self->frame_parameters[self->num_frames-1].a;
+
+  g_free(common_parameters);
+  self->direction = 1;
+
+  self->idler = g_idle_add(screensaver_idle_handler, self);
+}
+
+
+/************************************************************************************/
+/************************************************************************ Rendering */
+/************************************************************************************/
+
+static int screensaver_idle_handler(gpointer user_data) {
+  ScreenSaver *self = SCREENSAVER(user_data);
+
+  iterative_map_calculate_motion(ITERATIVE_MAP(self->frame_renders[self->current_frame]), 100000,
+				 TRUE, PARAMETER_INTERPOLATOR(parameter_holder_interpolate_linear),
+				 &self->frame_parameters[self->current_frame]);
+
+
+  histogram_view_set_imager(HISTOGRAM_VIEW(self->view),
+			    HISTOGRAM_IMAGER(self->frame_renders[self->current_frame]));
+  histogram_view_update(HISTOGRAM_VIEW(self->view));
+
+  self->current_frame += self->direction;
+  if (self->current_frame >= self->num_frames) {
+    self->current_frame = self->num_frames-1;
+    self->direction = -1;
+  }
+  if (self->current_frame < 0) {
+    self->current_frame = 0;
+    self->direction = 1;
+  }
+
+  return 1;
+}
+
+/* The End */
---
+++ src/screensaver.h
@@ -0,0 +1,75 @@
+/*
+ * screensaver.h - A self-running Fyre screen saver
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#ifndef __SCREENSAVER_H__
+#define __SCREENSAVER_H__
+
+#include <gtk/gtk.h>
+#include "animation.h"
+#include "de-jong.h"
+
+G_BEGIN_DECLS
+
+#define SCREENSAVER_TYPE            (screensaver_get_type ())
+#define SCREENSAVER(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), SCREENSAVER_TYPE, ScreenSaver))
+#define SCREENSAVER_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), SCREENSAVER_TYPE, ScreenSaverClass))
+#define IS_SCREENSAVER(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), SCREENSAVER_TYPE))
+#define IS_SCREENSAVER_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), SCREENSAVER_TYPE))
+
+typedef struct _ScreenSaver      ScreenSaver;
+typedef struct _ScreenSaverClass ScreenSaverClass;
+
+struct _ScreenSaver {
+  GObject object;
+
+  IterativeMap *map;
+  Animation *animation;
+
+  gdouble framerate;
+  IterativeMap **frame_renders;
+  ParameterHolderPair *frame_parameters;
+  int num_frames, current_frame;
+  int direction;
+
+  GtkWidget *window;
+  GtkWidget *view;
+
+  guint idler;
+};
+
+struct _ScreenSaverClass {
+  GObjectClass parent_class;
+};
+
+
+/************************************************************************************/
+/******************************************************************* Public Methods */
+/************************************************************************************/
+
+GType         screensaver_get_type();
+ScreenSaver*  screensaver_new(IterativeMap *map, Animation *animation);
+
+G_END_DECLS
+
+#endif /* __SCREENSAVER_H__ */
+
+/* The End */
