237efe3e933804210c086d1f8c36761222af9b3f
David Trowbridge <trowbrds@gmail.com>
2004-11-30 22:20:03 +0000

    make cset test the hosts
    git-svn-id: http://svn.navi.cx/misc/trunk/fyre@5937 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- contrib/cluster-utils.sh
+++ contrib/cluster-utils.sh
@@ -9,8 +9,18 @@
 # Set the current cluster- argument is a file of hostnames
 cset () {
     HOSTFILE=`pwd`/$1
-    HOST_LINES=`cat $HOSTFILE`
-    HOSTS=`perl -e '$_=join(",",<>);s/\s//g;print' $HOSTFILE`
+    HOST_LINES=""
+    for host in `cat $HOSTFILE`; do
+        echo -n "testing $host..."
+        ping -c 1 $host >&/dev/null
+        if [ "$?" -eq "0" ]; then
+            echo "ok!"
+	    HOST_LINES="$HOST_LINES $host"
+        else
+	    echo "down"
+        fi
+    done
+    HOSTS=`echo $HOST_LINES | tr ' ' ','`
 
     export HOSTS
     export HOST_LINES
