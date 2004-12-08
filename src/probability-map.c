f38bf5d09dbe448133a35c7e8ba8956c26928006
Micah Elizabeth Scott <beth@scanlime.org>
2004-12-08 02:25:37 +0000

    Add a new ProbabilityMap object, for creating 2D random variables with probability distributions described by image files. This can be used for image-mapped initial conditions, but the changes to do so are still too experimental to go in quite yet. Currently this is fairly fast, but still requires a binary search for every iteration. The binary search might be optimizable further, or we may be able to use a hash table of some sort.
    git-svn-id: http://svn.navi.cx/misc/trunk/fyre@5973 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ src/probability-map.c
@@ -0,0 +1,304 @@
+/* -*- mode: c; c-basic-offset: 4; -*-
+ *
+ * probability-map.c - A 2-D random variable with a probability
+ *                     distribution function defined as an image.
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#include <gtk/gtk.h>
+#include "probability-map.h"
+#include "math-util.h"
+
+static void probability_map_class_init(ProbabilityMapClass *klass);
+static void probability_map_dispose(GObject *gobject);
+
+
+/************************************************************************************/
+/**************************************************** Initialization / Finalization */
+/************************************************************************************/
+
+GType probability_map_get_type(void)
+{
+    static GType obj_type = 0;
+
+    if (!obj_type) {
+	static const GTypeInfo obj_info = {
+	    sizeof(ProbabilityMapClass),
+	    NULL, /* base_init */
+	    NULL, /* base_finalize */
+	    (GClassInitFunc) probability_map_class_init,
+	    NULL, /* class_finalize */
+	    NULL, /* class_data */
+	    sizeof(ProbabilityMap),
+	    0,
+	    NULL, /* instance init */
+	};
+
+	obj_type = g_type_register_static(G_TYPE_OBJECT, "ProbabilityMap", &obj_info, 0);
+    }
+
+    return obj_type;
+}
+
+static void probability_map_class_init(ProbabilityMapClass *klass)
+{
+    GObjectClass *object_class = (GObjectClass*) klass;
+    object_class->dispose = probability_map_dispose;
+}
+
+static void probability_map_dispose(GObject *gobject)
+{
+    ProbabilityMap *self = PROBABILITY_MAP(gobject);
+
+    if (self->cumulative) {
+	g_free(self->cumulative);
+	self->cumulative = NULL;
+    }
+}
+
+
+/************************************************************************************/
+/********************************************************************* Constructors */
+/************************************************************************************/
+
+ProbabilityMap*  probability_map_new_file            (const gchar*          filename)
+{
+    return probability_map_new_file_channel(filename, FYRE_CHANNEL_LUMA);
+}
+
+ProbabilityMap*  probability_map_new_file_channel    (const gchar*          filename,
+						      FyreImageChannel      channel)
+{
+    GdkPixbuf *pixbuf = gdk_pixbuf_new_from_file(filename, NULL);
+    ProbabilityMap *self;
+
+    g_assert(pixbuf != NULL);
+    self = probability_map_new_pixbuf_channel(pixbuf, channel);
+    gdk_pixbuf_unref(pixbuf);
+
+    return self;
+}
+
+ProbabilityMap*  probability_map_new_pixbuf          (GdkPixbuf*            pixbuf)
+{
+    return probability_map_new_pixbuf_channel(pixbuf, FYRE_CHANNEL_LUMA);
+}
+
+ProbabilityMap*  probability_map_new_pixbuf_channel  (GdkPixbuf*            pixbuf,
+						      FyreImageChannel      channel)
+{
+    int offset;
+
+    g_assert(gdk_pixbuf_get_colorspace(pixbuf) == GDK_COLORSPACE_RGB);
+    g_assert(gdk_pixbuf_get_bits_per_sample(pixbuf) == 8);
+
+    switch (channel) {
+
+    case FYRE_CHANNEL_RED:
+	offset = 0;
+	break;
+
+    case FYRE_CHANNEL_GREEN:
+	offset = 1;
+	break;
+
+    case FYRE_CHANNEL_BLUE:
+	offset = 2;
+	break;
+
+    case FYRE_CHANNEL_ALPHA:
+	g_assert(gdk_pixbuf_get_has_alpha(pixbuf));
+	offset = 3;
+	break;
+
+    default:
+	g_warning("Unsupported channel");
+	offset = 0;
+    }
+
+    return probability_map_new_raw(gdk_pixbuf_get_pixels(pixbuf) + offset,
+				   gdk_pixbuf_get_width(pixbuf),
+				   gdk_pixbuf_get_height(pixbuf),
+				   gdk_pixbuf_get_rowstride(pixbuf),
+				   gdk_pixbuf_get_n_channels(pixbuf),
+				   G_TYPE_UCHAR);
+}
+
+ProbabilityMap*  probability_map_new_raw          (const guchar*         data,
+						   gint                  width,
+						   gint                  height,
+						   gint                  row_stride,
+						   gint                  pixel_stride,
+						   gint                  pixel_type)
+{
+    ProbabilityMap *self = PROBABILITY_MAP(g_object_new(probability_map_get_type(), NULL));
+    gdouble sum = 0;
+    const guchar* row = data;
+    const guchar* pixel;
+    int x;
+    int y = height;
+    gfloat *output;
+
+    self->width = width;
+    self->height = height;
+    self->image_scale_x = 1.0 / (width - 1);
+    self->image_scale_y = 1.0 / (height - 1);
+
+    self->cumulative_length = width * height;
+    self->cumulative = g_new(gfloat, self->cumulative_length);
+    output = self->cumulative;
+
+    /* Caluclate a cumulative distribution. For each pixel, we save the
+     * sum of it and all prior pixels. For speed, these inner loops are
+     * implemented separately for each pixel type using a macro.
+     */
+
+#define CALCULATE_SUMS(type) do {       \
+        while (y) {                     \
+	    x = width;                  \
+	    pixel = row;                \
+	    while (x) {                 \
+		sum += *(type*)pixel;   \
+		*output = sum;          \
+		output++;               \
+		x--;                    \
+		pixel += pixel_stride;  \
+	    }                           \
+	    y--;                        \
+	    row += row_stride;          \
+	};                              \
+    } while (0)
+
+    switch (pixel_type) {
+
+    case G_TYPE_UCHAR:
+	CALCULATE_SUMS(guchar);
+	break;
+
+    case G_TYPE_UINT:
+	CALCULATE_SUMS(guint);
+	break;
+
+    case G_TYPE_ULONG:
+	CALCULATE_SUMS(gulong);
+	break;
+
+    case G_TYPE_FLOAT:
+	CALCULATE_SUMS(gfloat);
+	break;
+
+    case G_TYPE_DOUBLE:
+	CALCULATE_SUMS(gdouble);
+	break;
+
+    default:
+	g_warning("Unsupported pixel format");
+
+    }
+
+#undef CALCULATE_SUMS
+
+    return self;
+}
+
+
+/************************************************************************************/
+/******************************************************************* Public Methods */
+/************************************************************************************/
+
+void             probability_map_ints             (ProbabilityMap*       self,
+						   guint*                x,
+						   guint*                y)
+{
+    gfloat key;
+    int index;
+    gfloat* cumulative = self->cumulative;
+    gsize length = self->cumulative_length;
+    gsize width = self->width;
+    int endpoints[2];
+    int midpoints[2];
+
+    /* Start with a uniform variate, scaled to cover the range of our CDF */
+    key = uniform_variate() * cumulative[length - 1];
+
+    /* Do a binary search for the first value in 'cumulative' that's greater than
+     * or equal to 'key'. These semantics are necessary to ensure that in a run
+     * of values with the same cumulative value, only the first one is ever chosen
+     * since the other values have zero probability in the original image.
+     */
+    endpoints[0] = 0;
+    endpoints[1] = length - 1;
+    while (endpoints[1] > endpoints[0]) {
+	midpoints[0] = (endpoints[0] + endpoints[1]) / 2;
+	midpoints[1] = midpoints[0] + 1;
+
+	if (cumulative[midpoints[0]] < key &&
+	    cumulative[midpoints[1]] >= key) {
+	    /* We're sitting right on the stopping condition */
+	    break;
+	}
+	else if (cumulative[midpoints[1]] < key) {
+	    /* The solution is above this */
+	    endpoints[0] = midpoints[1];
+	}
+	else if (cumulative[midpoints[0]] >= key) {
+	    /* The solution is below this */
+	    endpoints[1] = midpoints[0];
+	}
+	else {
+	    g_assert(0);
+	}
+    }
+
+    /* Convert an index within our cumulative array into (x,y) coords */
+    *x = midpoints[1] % width;
+    *y = midpoints[1] / width;
+}
+
+void             probability_map_normalized       (ProbabilityMap*       self,
+						   gdouble*              x,
+						   gdouble*              y)
+{
+    guint xi, yi;
+    probability_map_ints(self, &xi, &yi);
+    *x = xi * self->image_scale_x;
+    *y = yi * self->image_scale_y;
+}
+
+void             probability_map_uniform          (ProbabilityMap*       self,
+						   gdouble*              x,
+						   gdouble*              y)
+{
+    probability_map_normalized(self, x, y);
+    *x += uniform_variate() * self->image_scale_x;
+    *y += uniform_variate() * self->image_scale_y;
+}
+
+void             probability_map_gaussian         (ProbabilityMap*       self,
+						   gdouble*              x,
+						   gdouble*              y,
+						   double                radius)
+{
+    probability_map_normalized(self, x, y);
+    *x += normal_variate() * self->image_scale_x * radius;
+    *y += normal_variate() * self->image_scale_y * radius;
+}
+
+/* The End */
---
+++ src/probability-map.h
@@ -0,0 +1,134 @@
+/* -*- mode: c; c-basic-offset: 4; -*-
+ *
+ * probability-map.h - A 2-D random variable with a probability
+ *                     distribution function defined as an image.
+ *
+ * Fyre - rendering and interactive exploration of chaotic functions
+ * Copyright (C) 2004 David Trowbridge and Micah Dowty
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+#ifndef __PROBABILITY_MAP_H__
+#define __PROBABILITY_MAP_H__
+
+#include <gtk/gtk.h>
+
+G_BEGIN_DECLS
+
+#define PROBABILITY_MAP_TYPE            (probability_map_get_type ())
+#define PROBABILITY_MAP(obj)            (G_TYPE_CHECK_INSTANCE_CAST ((obj), PROBABILITY_MAP_TYPE, ProbabilityMap))
+#define PROBABILITY_MAP_CLASS(klass)    (G_TYPE_CHECK_CLASS_CAST ((klass), PROBABILITY_MAP_TYPE, ProbabilityMapClass))
+#define IS_PROBABILITY_MAP(obj)         (G_TYPE_CHECK_INSTANCE_TYPE ((obj), PROBABILITY_MAP_TYPE))
+#define IS_PROBABILITY_MAP_CLASS(klass) (G_TYPE_CHECK_CLASS_TYPE ((klass), PROBABILITY_MAP_TYPE))
+
+typedef struct _ProbabilityMap         ProbabilityMap;
+typedef struct _ProbabilityMapClass    ProbabilityMapClass;
+
+struct _ProbabilityMap {
+    GObject object;
+
+    /* Public read-only */
+
+    int     width;
+    int     height;
+
+    /* Private */
+
+    gfloat* cumulative;           /* 1D cumulative distribution function */
+    gsize   cumulative_length;
+
+    gdouble image_scale_x;
+    gdouble image_scale_y;
+};
+
+struct _ProbabilityMapClass {
+    GObjectClass parent_class;
+};
+
+typedef enum {
+    FYRE_CHANNEL_RED,
+    FYRE_CHANNEL_GREEN,
+    FYRE_CHANNEL_BLUE,
+    FYRE_CHANNEL_ALPHA,
+    FYRE_CHANNEL_LUMA,
+} FyreImageChannel;
+
+
+/************************************************************************************/
+/******************************************************************* Public Methods */
+/************************************************************************************/
+
+GType            probability_map_get_type            ();
+
+/* Create a new probability map from a file, automatically identifying its type. */
+ProbabilityMap*  probability_map_new_file            (const gchar*          filename);
+
+/* Create a new probability map from a file, automatically identifying its type.
+ * Uses data from the indicated channel.
+ */
+ProbabilityMap*  probability_map_new_file_channel    (const gchar*          filename,
+						      FyreImageChannel      channel);
+
+/* Create a new probability map from a GdkPixbuf. */
+ProbabilityMap*  probability_map_new_pixbuf          (GdkPixbuf*            pixbuf);
+
+/* Create a new probability map from a GdkPixbuf. Uses data from the indicated channel */
+ProbabilityMap*  probability_map_new_pixbuf_channel  (GdkPixbuf*            pixbuf,
+						      FyreImageChannel      channel);
+
+/* Create a new probability map from a raw image, given its pixel and row strides,
+ * and the GType used for each pixel sample. The data does not need to remain valid
+ * after this call completes, the caller can free it immediately.
+ *
+ * Currently, pixel types of G_TYPE_UCHAR, G_TYPE_UINT, G_TYPE_ULONG, G_TYPE_FLOAT,
+ * and G_TYPE_DOUBLE are valid.
+ */
+ProbabilityMap*  probability_map_new_raw          (const guchar*         data,
+						   gint                  width,
+						   gint                  height,
+						   gint                  row_stride,
+						   gint                  pixel_stride,
+						   gint                  pixel_type);
+
+/* There are several functions for retrieving new random values from the
+ * probability map: integer pixel coordinates, normalized (0 to 1) floating
+ * point coordinates with no filtering, uniform filtering, or gaussian filtering.
+ *
+ * With no filtering, the results will always lie on pixel boundaries. With
+ * uniform filtering, each pixel is treated as a square uniform distribution.
+ * With gaussian filtering, each pixel is the center of a small gaussian distribution
+ * with a given standard deviation.
+ */
+void             probability_map_ints             (ProbabilityMap*       self,
+						   guint*                x,
+						   guint*                y);
+void             probability_map_normalized       (ProbabilityMap*       self,
+						   gdouble*              x,
+						   gdouble*              y);
+void             probability_map_uniform          (ProbabilityMap*       self,
+						   gdouble*              x,
+						   gdouble*              y);
+void             probability_map_gaussian         (ProbabilityMap*       self,
+						   gdouble*              x,
+						   gdouble*              y,
+						   double                radius);
+
+G_END_DECLS
+
+#endif /* __PROBABILITY_MAP_H__ */
+
+/* The End */
