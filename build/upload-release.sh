a12d301eda68fd63760d5a660520918f69144dde
Micah Elizabeth Scott <beth@scanlime.org>
2005-02-06 23:11:40 +0000

    Add a script that automatically files the output of dist-win32.sh on flapjack
    git-svn-id: http://svn.navi.cx/misc/trunk/fyre@6751 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

---
+++ build/upload-release.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#
+# After running 'dist-win32.sh' to build the windows
+# releases, this uploads them to our release server.
+# It automatically chooses a different destination
+# directory for svn snapshots.
+#
+
+OUTPUTDIR=output
+RELEASE_BASE=flapjack:/var/www/localhost/htdocs/releases/fyre
+SVN_RELEASE_BASE=$RELEASE_BASE/win32-beta
+
+for filename in `cd $OUTPUTDIR; ls`; do
+
+    TARGET=$RELEASE_BASE
+    if (echo $filename | grep -q svn); then
+	TARGET=$SVN_RELEASE_BASE
+    fi
+
+    scp $OUTPUTDIR/$filename $TARGET/$filename
+
+done
+
+### The End ###
