9104d168111f20cee7467c88a6a23f3b989f77ac
David Trowbridge <trowbrds@gmail.com>
2005-07-31 21:46:19 +0000

    10000x10000 pixels just isn't big enough
    git-svn-id: http://svn.navi.cx/misc/branches/fyre-1.0@8774 9a66fdd2-3ab9-0310-9df6-91dd0304fdbc

--- src/histogram-imager.c
+++ src/histogram-imager.c
@@ -133,7 +133,7 @@ histogram_imager_init_size_params (GObjectClass *object_class)
     spec = g_param_spec_uint         ("width",
 				      "Width",
 				      "Width of the rendered image, in pixels",
-				      1, 10000, 600,
+				      1, 32767, 600,
 				      G_PARAM_READWRITE | G_PARAM_CONSTRUCT | PARAM_SERIALIZED | PARAM_IN_GUI);
     param_spec_set_group             (spec, current_group);
     param_spec_set_increments        (spec, 1, 16, 0);
@@ -142,7 +142,7 @@ histogram_imager_init_size_params (GObjectClass *object_class)
     spec = g_param_spec_uint         ("height",
 				      "Height",
 				      "Height of the rendered image, in pixels",
-				      1, 10000, 600,
+				      1, 32767, 600,
 				      G_PARAM_READWRITE | G_PARAM_CONSTRUCT | PARAM_SERIALIZED | PARAM_IN_GUI);
     param_spec_set_group             (spec, current_group);
     param_spec_set_increments        (spec, 1, 16, 0);
